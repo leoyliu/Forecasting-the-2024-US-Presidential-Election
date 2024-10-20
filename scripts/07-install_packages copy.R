8b9633d8b74c6591860504025fe97fb120b2c5a3
Leo Liu <liuyuanyi1208@gmail.com>
2024-10-20 17:40:59 -0400

    Update paper and delete unnecessary files

---
+++ scripts/07-install_packages copy.R
@@ -0,0 +1,17 @@
+#### Preamble ####
+# Purpose: Install necessary packages
+# Author: Yuanyi (Leo) Liu, Dezhen Chen, Ziyuan Shen
+# Date: 26 September 2024
+# Contact: leoy.liu@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: None
+# Any other information needed? None
+
+
+#### Install packages ####
+install.packages("tidyverse")
+install.packages("arrow")
+install.packages("here")
+install.packages("kableExtra")
+install.packages("opendatatoronto")
+install.packages("janitor")
