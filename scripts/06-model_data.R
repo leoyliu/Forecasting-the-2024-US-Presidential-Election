1e77f453b0cd5d95979488199fdc045c02af5f23
Dezhen Chen <dezhenchenpeter@gmail.com>
2024-10-26 19:40:27 -0400

    update model

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -11,23 +11,38 @@
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(rsample)
+library(dplyr)
+
 
 #### Read data ####
 analysis_data <- read_csv("data/02-analysis_data/analysis_data.csv")
+analysis_data_test <- read_parquet("data/02-analysis_data/test_data.parquet")
+analysis_data_train <- read_parquet("data/02-analysis_data/train_data.parquet")
 
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = pct ~ poll_type + pollster + pollscore + transparency_score,
-    data = analysis_data,
+    formula = pct~ numeric_grade + as.factor(methodology) + duration + transparency_score+state
+    ,
+    data = analysis_data_train,
     family = gaussian(),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
     seed = 853
   )
-
 summary(first_model)
+
+vif(first_model)
+
+m2 <-lm(pct~ numeric_grade + as.factor(methodology) + duration +sample_size + transparency_score+ state,data=analysis_data_train)
+
+summary(m2)
+
+vif(m2)
+
+
 
 
 #### Save model ####
