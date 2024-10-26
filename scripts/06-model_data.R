fcaa64a88ec523037bd8e53437838f3a93797261
Leo Liu <liuyuanyi1208@gmail.com>
2024-10-26 19:44:15 -0400

    fix formatting in model

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -13,6 +13,7 @@ library(tidyverse)
 library(rstanarm)
 library(rsample)
 library(dplyr)
+library(car)
 
 
 #### Read data ####
@@ -20,11 +21,11 @@ analysis_data <- read_csv("data/02-analysis_data/analysis_data.csv")
 analysis_data_test <- read_parquet("data/02-analysis_data/test_data.parquet")
 analysis_data_train <- read_parquet("data/02-analysis_data/train_data.parquet")
 
+
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = pct~ numeric_grade + as.factor(methodology) + duration + transparency_score+state
-    ,
+    formula = pct~ numeric_grade + as.factor(methodology) + duration + transparency_score + state,
     data = analysis_data_train,
     family = gaussian(),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
@@ -33,20 +34,16 @@ first_model <-
     seed = 853
   )
 summary(first_model)
-
 vif(first_model)
 
-m2 <-lm(pct~ numeric_grade + as.factor(methodology) + duration +sample_size + transparency_score+ state,data=analysis_data_train)
-
-summary(m2)
-
-vif(m2)
-
-
+model2 <-lm(pct~ numeric_grade + as.factor(methodology) + duration + sample_size + transparency_score + state, 
+            data = analysis_data_train)
+summary(model2)
+vif(model2)
 
 
 #### Save model ####
 saveRDS(
-  first_model,
+  model2,
   file = "models/first_model.rds"
 )
