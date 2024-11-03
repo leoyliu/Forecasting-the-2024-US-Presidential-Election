7fe0beea90876ea201b35d107f6eea34e89e0f45
Leo Liu <liuyuanyi1208@gmail.com>
2024-11-03 03:02:55 -0500

    update model, paper, scripts

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -16,29 +16,32 @@ library(tidyverse)
 #### Clean data ####
 raw_data <- read_csv("data/01-raw_data/raw_data.csv")
 
-legal_marijuana_states <- c(
-  "Colorado", "Washington", "Alaska", "Oregon", "Washington, D.C.", "California",
-  "Maine", "Massachusetts", "Nevada", "Michigan", "Vermont", "Guam", "Illinois", 
-  "Arizona", "Montana", "New Jersey", "New York", "Virginia", "New Mexico", 
-  "Connecticut", "Rhode Island", "Maryland", "Missouri", "Delaware", "Minnesota", "Ohio"
-)
-
 cleaned_data <- 
   raw_data |> 
   janitor::clean_names() |> 
   filter(numeric_grade >= 2.5, candidate_name == "Donald Trump") |> 
-  mutate(end_date = mdy(end_date)) |>
+  mutate(
+    end_date = mdy(end_date),
+    state = case_when(              # Aggregate sub-regions into main states
+      state %in% c("Nebraska", "Nebraska CD-2") ~ "Nebraska",
+      state %in% c("Maine", "Maine CD-1", "Maine CD-2") ~ "Maine",
+      TRUE ~ state
+    )
+  ) |>
   filter(end_date >= as.Date("2024-07-15")) |>  # Trump announced joining campaign
   mutate(
     duration = as.numeric(difftime(end_date, min(end_date), units = "days")),  # New column: duration (end_date - start_date of Trump)
     num_trump = round((pct / 100) * sample_size, 0),  # Number of respondents supporting Trump
-    methodology = ifelse(str_detect(methodology, "/"), "Mixed Voting", methodology),  # Replace "/" with "multiple ways of voting"
-    marijuana_legal = ifelse(state %in% legal_marijuana_states, 1, 0)  # Map whether the state allows marijuana legally
-  ) |> 
+    methodology = case_when(
+      str_detect(methodology, "/") ~ "Mixed Voting",
+      methodology == "Email" ~ "Online Panel",   # Aggregate Email to Online Panel
+      methodology == "IVR" ~ "Live Phone",        # Aggregate IVR to Live Phone
+      TRUE ~ methodology
+    )) |> 
   select(
     poll_id, numeric_grade, methodology, transparency_score, 
-    end_date, sample_size, pollster_name = display_name,  # Rename display_name to pollster_name
-    state, pct, duration, num_trump, marijuana_legal
+    end_date, sample_size, pollster_name = display_name,
+    state, pct, duration, num_trump, 
   ) |> 
   distinct() |> 
   drop_na() # Omit rows with any NA values
--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -17,33 +17,24 @@ library(car)
 library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("data/02-analysis_data/analysis_data.csv")
-analysis_data_test <- read_parquet("data/02-analysis_data/test_data.parquet")
 analysis_data_train <- read_parquet("data/02-analysis_data/train_data.parquet")
 
 
 ### Model data ####
-first_model <-
+model <-
   stan_glm(
-    formula = pct~ numeric_grade + as.factor(methodology) + duration + transparency_score + state,
+    formula = pct ~ numeric_grade + as.factor(methodology) + duration + sample_size + transparency_score + state,
     data = analysis_data_train,
     family = gaussian(),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
     seed = 853
-  )
-summary(first_model)
-vif(first_model)
-
-model2 <-lm(pct~ numeric_grade + as.factor(methodology) + duration + sample_size + transparency_score + state, 
-            data = analysis_data_train)
-summary(model2)
-vif(model2)
+)
 
 
 #### Save model ####
 saveRDS(
-  model2,
+  model,
   file = "models/first_model.rds"
 )
